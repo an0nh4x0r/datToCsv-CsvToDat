e006d95a6fad4c42089d4c778f853aefe80f7c18
swapnil singh <swappp7singh@gmail.com>
2018-08-15 00:48:53 +0530

    Now data from dat file is getting stored in the excel file

--- src/DatToCsv.java
+++ src/DatToCsv.java
@@ -1,11 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class DatToCsv {
 
-    private static final String DELIMITER = "|";
+    private static final String DELIMITER = "\\|";
     private static final String INPUT_FILE = "src/SomeDatFile.dat";
+    private static final String OUTPUT_FILE = "src/GeneratedExcelFile.csv";
 
     public static void main(String[] args) {
         String line = null;
@@ -14,11 +13,25 @@ public class DatToCsv {
             FileReader fileReader = new FileReader(INPUT_FILE);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            while((line = bufferedReader.readLine()) != null) {
-                System.out.println(line);
+            PrintWriter pw = new PrintWriter(new File(OUTPUT_FILE));
+            StringBuilder sb = new StringBuilder();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] tokens = line.split(DELIMITER);
+                for (int i = 0; i < tokens.length; ++i) {
+                    if (i != tokens.length - 1) {
+                        sb.append(tokens[i] + ",");
+                    }
+                    else {
+                        sb.append(tokens[i] + "\n");
+                    }
+                }
             }
 
+            pw.write(sb.toString());
+
             bufferedReader.close();
+            pw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
